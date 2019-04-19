2d8a2658367af4f8f83062bc39049ce8e9008df2
Michel Megens <dev@bietje.net>
2019-04-19 15:54:41 +0200

    monitor: implement the ability to monitor output
    Implement the monitor target which only attempts to monitor the serial
    port.
    
    Signed-off-by: Michel Megens <dev@bietje.net>

--- common.mk
+++ common.mk
@@ -261,6 +261,9 @@ size: $(PROGRAM_OUT)
 test: flash
 	$(FILTEROUTPUT) --port $(ESPPORT) --baud 115200 --elf $(PROGRAM_OUT)
 
+monitor:
+	$(FILTEROUTPUT) --port $(ESPPORT) --baud 115200 --elf $(PROGRAM_OUT)
+
 # the rebuild target is written like this so it can be run in a parallel build
 # environment without causing weird side effects
 rebuild:
